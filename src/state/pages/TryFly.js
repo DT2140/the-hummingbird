dba5bb8ad49e1a19b9068d11f7695de7b254cc18
Lon Hansson <sailorlon@gmail.com>
2017-01-03 16:16:26 +0100

    removed unused code

--- src/state/pages/TryFly.js
+++ src/state/pages/TryFly.js
@@ -10,7 +10,7 @@ export default class TryFly extends Page {
   }
 	
   create(){
-      
+     
     super.create();
 	const hummingbird = this.game.add.video('hummingbird');
 	
@@ -19,24 +19,10 @@ export default class TryFly extends Page {
     this.foreground.y += this.foreground.height / 10;
 			
 	hummingbird.addToWorld(400, 550, 0.5, 0.5, 0.5, 0.5);
-    hummingbird.alpha = 0;	
-	
-    this.queue('fadeInHummingbird', done => {
-	   	this.tweenImage(hummingbird, 1, 2000).onComplete.add(() => {
-	        this.isTweened = true;
-			done();
-		});
-    });
-	
+
 	this.queue('hummingbirdFly', done => {
 		hummingbird.play(true);
 		done();
     });
-  }
-//tween function
-  tweenImage(sprite, alpha, time) {
-    const tween = this.game.add.tween(sprite);
-	tween.to({alpha: alpha}, time, Phaser.Easing.Bounce.Out, true, 0);
-	return tween;
   }
 }
