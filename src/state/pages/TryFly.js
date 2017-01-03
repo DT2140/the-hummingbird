746e43824080ca06d39e1a1177723ac8b3377133
Nilay <nilaya@kth.se>
2017-01-03 14:52:58 +0100

    Added the images for the TryFly page

--- src/state/pages/TryFly.js
+++ src/state/pages/TryFly.js
@@ -2,19 +2,18 @@ import Page from './Page';
 
 export default class TryFly extends Page {
 	preload(){
-		this.game.load.image('sky','/assets/backgrounds/02-Sky.svg');
-		this.game.load.image('branch','/assets/foregrounds/02-Branch.svg');
-		this.game.load.image('bird','/assets/foregrounds/02-Son.Still.svg');
+
+		this.loadBackground('sky2.png');
+		this.loadForeground('branch.png');
+
 		//this.game.load.video('tryfly','/assets/animations/02.Son.TryFly.webm')
 	}
 	create(){
 		super.create();
 
-		const sky = this.game.add.sprite(0,0,'sky');
-		const branch = this.game.add.sprite(0,0,'branch');
-		const bird = this.game.add.sprite(-270,-200,'bird')
-		
-		//const tryfly = this.game.add.video(0,0,'tryfly')
-		//this.fillCenter(sky);
+		this.foreground.y = this.game.height - this.foreground.height;
+		this.foreground.x -= this.foreground.width / 10;
+		this.foreground.y += this.foreground.height / 10;
+
 	}
 }
