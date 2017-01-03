e7ebf9ca2ae2615d4b5c24c566337e6221468645
Lon Hansson <sailorlon@gmail.com>
2017-01-03 13:00:08 +0100

    fixed shit

--- src/state/pages/OnceUpon.js
+++ src/state/pages/OnceUpon.js
@@ -1,19 +1,44 @@
 import Page from './Page';
 
 export default class OnceUpon extends Page {
-preload(){
-	this.game.load.image('mountains','/assets/backgrounds/01-Mountains.svg');
-	this.game.load.image('sky','/assets/backgrounds/01-Sky.svg')
-	this.game.load.image('tree and lake','/assets/foregrounds/01-Tree & Lake.svg')
-}
-
-create(){
-	super.create();
-
-	const sky = this.game.add.sprite(0,0,'sky')
-	const background = this.game.add.sprite(0,0,'mountains')
-	const treelake = this.game.add.sprite(0,0,'tree and lake')
+preload() {  
+	//set sprites
+    this.loadBackground('sky.png');
+	this.loadMiddleground('mountain-wide.png');
+	this.loadForeground('branch.png');
+  }
 	
-	//this.fillCenter(mountains);
-	}
+  create () {
+	super.create();
+	
+	this.middleground.scale.setTo(1, 1);
+    const proportion = this.game.height / this.middleground.height;
+	 
+	this.middleground.scale.setTo(proportion, proportion);
+	
+	//offset position to get tween to end at right position
+	this.middleground.position.set(-200, 0);
+	this.foreground.position.set(-500, 0);
+
+	//check if tween is completed
+	this.isTweened = false;
+	
+	this.tweenImage(this.middleground, 0, 4000);
+	this.tweenImage(this.foreground, 0, 4000).onComplete.add(()=>{
+		this.isTweened = true;
+		
+		/*here we will:
+			1.fade in characters
+			2.activate the speech recognition
+		*/
+	});
+  }
+  
+  //tween function
+  tweenImage(sprite, position, time) {
+    const tween = this.game.add.tween(sprite);
+	tween.to({x: position}, time, 'Linear', true, 0);
+	return tween;
+  }
 }
+
