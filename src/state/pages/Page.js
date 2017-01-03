73c394dd09e496e389e42b002dd9e3ae8e31fa63
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-01-03 12:01:06 +0100

    Fixed indent

--- src/state/pages/Page.js
+++ src/state/pages/Page.js
@@ -15,22 +15,24 @@ export default class Page extends Phaser.State {
   }
 
   loadBackground(backgroundImage) {
-	  this.game.load.image('background', '/assets/backgrounds/'+backgroundImage);
-	  this.hasBackgroud = true;
+    this.game.load.image('background', '/assets/backgrounds/' + backgroundImage);
+    this.hasBackgroud = true;
   }
-  
+
   loadMiddleground(middlegroundImage) {
-	  this.game.load.image('middleground', '/asset/middlegrounds/'+middlegroundImage);
-	  this.hasMiddleground = true;
+    this.game.load.image('middleground', '/asset/middlegrounds/' + middlegroundImage);
+    this.hasMiddleground = true;
   }
-  
+
   loadForeground(foregroundImage) {
-	  this.game.load.image('foreground', '/asset/foregrounds/'+foregroundImage);
-	  this.hasForeground = true;
+    this.game.load.image('foreground', '/asset/foregrounds/' + foregroundImage);
+    this.hasForeground = true;
   }
-  
+
   fillCenter(sprite) {
-    const { game } = this;
+    const {
+      game
+    } = this;
     const proportion = game.width / sprite.width;
 
     sprite.scale.setTo(proportion, proportion);
@@ -39,20 +41,20 @@ export default class Page extends Phaser.State {
 
   create() {
     this.game.stage.backgroundColor = 0xffffff;
-	
-	if (this.hasBackgroud) {
-		this.background = this.game.add.sprite(0, 0, 'background');
-		this.fillCenter(this.background);
-	}
-	
-	if (this.hasMiddleground) {
-		this.middleground = this.game.add.sprite(0, 0, 'middleground');
-		this.fillCenter(this.middleground);
-	}
-	
-	if (this.hasForeground) {
-		this.foreground = this.game.add.sprite(0, 0, 'foreground');
-		this.fillCenter(this.foreground);
-	}
+
+    if (this.hasBackgroud) {
+      this.background = this.game.add.sprite(0, 0, 'background');
+      this.fillCenter(this.background);
+    }
+
+    if (this.hasMiddleground) {
+      this.middleground = this.game.add.sprite(0, 0, 'middleground');
+      this.fillCenter(this.middleground);
+    }
+
+    if (this.hasForeground) {
+      this.foreground = this.game.add.sprite(0, 0, 'foreground');
+      this.fillCenter(this.foreground);
+    }
   }
 }
