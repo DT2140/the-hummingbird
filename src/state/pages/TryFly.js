7943bb3c9785bc8de3a1c587582327439c3912d4
Nilay <nilaya@kth.se>
2017-01-03 16:09:04 +0100

    Removed the still image of hummingbird and added the video instead

--- src/state/pages/TryFly.js
+++ src/state/pages/TryFly.js
@@ -3,33 +3,34 @@ import Page from './Page';
 export default class TryFly extends Page {
   preload(){
 
-    //this.loadBackground('sky2.png');
+    this.loadBackground('sky2.png');
     this.loadForeground('branch.png');
-	this.game.load.image('hummingbird','/assets/characters/hummingbird-still.png');
-
-    //this.game.load.video('tryfly','/assets/animations/02.Son.TryFly.webm')
+	
+    this.game.load.video('hummingbird','/assets/animations/02.Son.TryFly.webm')
   }
 	
   create(){
       
     super.create();
-	const hummingbird = this.game.add.sprite(0,this.foreground.height / 10,'hummingbird');
-	  
+	const hummingbird = this.game.add.video('hummingbird');
+	
     this.foreground.y = this.game.height - this.foreground.height;
     this.foreground.x -= this.foreground.width / 10;
     this.foreground.y += this.foreground.height / 10;
 			
+	hummingbird.addToWorld(400, 550, 0.5, 0.5, 0.5, 0.5);
     hummingbird.alpha = 0;	
 	
     this.queue('fadeInHummingbird', done => {
-      this.tweenImage(hummingbird, 1, 2000).onComplete.add(() => {
-        this.isTweened = true;
-		done();
-	  });
+	   	this.tweenImage(hummingbird, 1, 2000).onComplete.add(() => {
+	        this.isTweened = true;
+			done();
+		});
     });
 	
 	this.queue('hummingbirdFly', done => {
-      // add video here
+		hummingbird.play(true);
+		done();
     });
   }
 //tween function
