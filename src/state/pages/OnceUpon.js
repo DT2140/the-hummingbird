275dbfb332703040ed41ebd3479d572710efde02
Lon Hansson <sailorlon@gmail.com>
2017-01-04 15:14:07 +0100

    nice pan on page 1 instead of bounce

--- src/state/pages/OnceUpon.js
+++ src/state/pages/OnceUpon.js
@@ -5,37 +5,34 @@ export default class OnceUpon extends Page {
     //set sprites
     
 	this.loadSky('sky.png');
-	//this.loadBackground('...');
-    this.loadMiddleground('mountains.png');
-    this.loadForeground('tree.png');
+	this.loadBackground('mountain-wide.png');
+    //this.loadMiddleground('...');
     //TODO add lake
+	this.loadForeground('tree.png');
   }
 
   create() {
     super.create();
 
-	
-    this.middleground.scale.setTo(1, 1);
-    this.middleground.x -=  (this.middleground.width - this.game.width ) / 2;
-    //const proportion = this.game.height / this.middleground.height;
-
-    //this.middleground.scale.setTo(proportion, proportion);
+    this.background.scale.setTo(1, 1);
+    this.background.x -=  (this.background.width - this.game.width ) / 2;
 
     this.isTweened = false;
 
-    this.foreground.position.set(0, -this.foreground.height);
+    this.foreground.position.set(-this.foreground.width, 0);
+	this.background.position.set(-this.background.width/10, 0);
+	
 	
     this.queue('showTree', done => {
-      this.tweenImage(this.foreground, 0, 2000).onComplete.add(() => {
-        this.isTweened = true;
-        done();
-      });
+      this.tweenImage(this.foreground, 0, 4000);
+	  this.tweenImage(this.background, 0, 4000);
+      done();  
 	})
   }
 //tween function
   tweenImage(sprite, position, time) {
     const tween = this.game.add.tween(sprite);
-	tween.to({y: position}, time, Phaser.Easing.Bounce.Out, true, 0);
+	tween.to({x: position}, time, 'Linear', true, 0);
 	return tween;
   }
 }
