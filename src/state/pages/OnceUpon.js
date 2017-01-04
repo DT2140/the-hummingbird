0a83b2b8f33c64f5bfdc28913b90279153481128
Nilay <nilaya@kth.se>
2017-01-04 18:24:09 +0100

    Made the lake image separate, so parallax looks nicer

--- src/state/pages/OnceUpon.js
+++ src/state/pages/OnceUpon.js
@@ -5,8 +5,8 @@ export default class OnceUpon extends Page {
     //set sprites
 
     this.loadSky('sky.png');
-    this.loadBackground('mountain-lake.png');
-//  this.loadMiddleground('...');
+    this.loadMiddleground('lake.png');
+    this.loadBackground('mountain.png');
     this.loadForeground('tree.png');
   }
 
@@ -14,15 +14,18 @@ export default class OnceUpon extends Page {
     super.create();
 
     this.background.scale.setTo(1, 1);
+    this.middleground.scale.setTo(1, 1);
 
     this.isTweened = false;
 
+    this.background.position.set(-this.background.width / 10, this.background.height / 10);
     this.foreground.position.set(-this.foreground.width, this.game.height - this.foreground.height);
-    this.background.position.set(-this.background.width / 10, 0);
+    this.middleground.position.set(-this.middleground.width / 5, this.background.y + this.background.height/10*9);
 
     this.queue('showTree', done => {
       this.tweenImage(this.foreground, 0, 4000);
       this.tweenImage(this.background, 0, 4000);
+      this.tweenImage(this.middleground, 0, 4000);
       done();
     });
   }
