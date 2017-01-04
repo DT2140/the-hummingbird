4617bb72f4f40e6dfeb17a1b4de29c627b9cfa89
Nilay <nilaya@kth.se>
2017-01-04 17:48:36 +0100

    Positioning of the branch and the hummingbird

--- src/state/pages/TryFly.js
+++ src/state/pages/TryFly.js
@@ -14,13 +14,13 @@ export default class TryFly extends Page {
 
     super.create();
     const hummingbird = this.game.add.video('hummingbird');
-/*
+
     this.foreground.y = this.game.height - this.foreground.height;
     this.foreground.x -= this.foreground.width / 10;
     this.foreground.y += this.foreground.height / 10;
-*/
+
     hummingbird.loop = false;
-    hummingbird.addToWorld(this.foreground.width/3, this.foreground.height/2, 0.5, 0.5, 0.25, 0.25);
+	hummingbird.addToWorld(this.foreground.x + this.foreground.width / 2, this.foreground.y, 0.5, 0.5, 0.25, 0.25);
 
     this.queue('hummingbirdFly', done => {
       hummingbird.play(true);
